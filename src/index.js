6d17cd20725d65078bd6a39989aa350e3983b051
Alex <alexmatvichuk@gmail.com>
2021-06-20 19:13:41 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+import Splide from '@splidejs/splide';
+
+new Splide('.splide', {
+  type: 'loop',
+  perPage: 3,
+}).mount();
+var elms = document.getElementsByClassName('splide');
+for (var i = 0, len = elms.length; i < len; i++) {
+  new Splide(elms[i]).mount();
+}
